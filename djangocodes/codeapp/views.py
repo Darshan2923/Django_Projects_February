ed7d296bc324454c7164000c896ef8b502c40bac
Darshan <darshanpatel2923@gmail.com>
2024-01-03 09:35:35 +0530

    Corrected the auth code for the project

--- djangocodes/codeapp/views.py
+++ djangocodes/codeapp/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.models import User
+from .models import *
 # Create your views here.
 def index(request):
-    context={}
+    posts = BlogPost.objects.all()
+    posts = BlogPost.objects.filter().order_by('-datetime')
+    context={'posts':posts}
     return render(request,'codeapp/index.html',context)
 
 # User authentication
-
 def register_page(request):
     if request.method=="POST":   
         username = request.POST['username']
@@ -17,6 +19,10 @@ def register_page(request):
         last_name=request.POST['last_name']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
+
+        if User.objects.filter(username = username).first():
+            messages.error(request, "This username is already taken")
+            return redirect('index')
         
         if password1 != password2:
             messages.error(request, "Passwords do not match.")
@@ -26,11 +32,8 @@ def register_page(request):
         user.first_name = first_name
         user.last_name = last_name
         user.save()
-        return render(request, 'blogapp/login_page.html')   
-        # user_obj=User.objects.user(username=username)
-        # user_obj.set_password(password)
-
-    return render(request,'blogapp/register_page.html')
+        return render(request, 'codeapp/login_page.html')   
+    return render(request,'codeapp/register_page.html')
 
 
 def login_page(request):
