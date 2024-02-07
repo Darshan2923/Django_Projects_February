af0d0298cbab7a3e7e9508c85d496ddf7c861356
Darshan <darshanpatel2923@gmail.com>
2024-02-07 22:32:40 +0530

    importing models and functionalities in the views.py

--- donion/onionapp/views.py
+++ donion/onionapp/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
+import requests
+from django.shortcuts import render, redirect
+from bs4 import BeautifulSoup as BSoup
+from .models import Headline
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Print hello world')
+    context={}
+    return render(request,'onionapp/index.html',context)
